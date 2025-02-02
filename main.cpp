b7d64f944271ac5ab4777a42cd11c620f75d8b10
allan-koech <allan.koech1@outlook.com>
2025-02-02 18:08:07 +0300

    feat: add function to accept and validate deposit from customer

--- main.cpp
+++ main.cpp
@@ -2,3 +2,35 @@
 #include<string>
 #include<typeinfo>
 
+// 1. Accept and update deposit from the customer;
+// 2. Get the number of bets from the customer
+// 3. Spin the wheel
+// 4. Check if customer won
+// 5. Give the customer winnings
+// 6. ask the customer to play again.
+
+int collectDeposit(){
+   int depositAmount;
+   std:: cout << "\n Welcome to Kingdom bets";
+   bool checkDeposit = true;
+   while(checkDeposit) {
+      int proposedDeposit;
+      std:: cout << " \n Enter the amount you would like to deposit";
+      std:: cin >> proposedDeposit;
+      if(proposedDeposit > 0){
+         depositAmount = proposedDeposit;
+        std:: cout << " Deposit successful";
+        checkDeposit = false;
+        break;
+      } else{
+         std:: cout << "\n invalid amount please try again";}
+   }
+   return 0;
+};
+
+
+
+int main(){
+   collectDeposit();
+   return 0;
+}
