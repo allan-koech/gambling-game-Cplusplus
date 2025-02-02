1a0f7e45cb342abc4f43f7ba668ee6f3eb0aaed4
allan-koech <allan.koech1@outlook.com>
2025-02-02 19:41:19 +0300

    feat: collect and validate bet amount

--- main.cpp
+++ main.cpp
@@ -2,35 +2,67 @@
 #include<string>
 #include<typeinfo>
 
+ using namespace std;
+
 // 1. Accept and update deposit from the customer;
 // 2. Get the number of bets and the bet amount from the customer
 // 3. Spin the wheel
 // 4. Check if customer won
 // 5. Give the customer winnings
 // 6. ask the customer to play again.
-
+int balance;
 int collectDeposit(){
    int depositAmount;
-   std:: cout << "\n Welcome to Kingdom bets";
+   cout << "\n Welcome to Kingdom bets";
    bool checkDeposit = true;
    while(checkDeposit) {
       int proposedDeposit;
-      std:: cout << " \n Enter the amount you would like to deposit";
-      std:: cin >> proposedDeposit;
+      cout << "Enter the amount you would like to deposit  \n ";
+      cin >> proposedDeposit;
       if(proposedDeposit > 0){
          depositAmount = proposedDeposit;
-        std:: cout << " Deposit successful";
+         balance += depositAmount;
+        cout << " Deposit successful \n";
         checkDeposit = false;
         break;
       } else{
-         std:: cout << "\n invalid amount please try again";}
+       cout << " invalid amount please try again \n";}
    }
    return 0;
 };
+int getBetAmount(){
+double betPerLine;
+int numberOfLines;
+double totalBetAmount;
+bool checkAmount = true;
+do{
+cout << "Enter the number of Lines you would like to bet on";
+cin >> numberOfLines;
+if (numberOfLines < 1 || numberOfLines > 3) {
+            cout << "Invalid input! Please enter a number between 1 and 3.";
+        }
+}while ( numberOfLines < 1 ||numberOfLines > 3 );
+
+do{
+   cout << "Enter the amount you would like to bet on per line ";
+   cin >> betPerLine;
+   totalBetAmount = betPerLine * numberOfLines;
+   cout << totalBetAmount;
+   if(totalBetAmount > balance || betPerLine < 0 ){
+      cout << "Invalid bet Amount! Please try again \n";
+   } else {
+      cout << "Bet Placed Successfully";
+      balance -= totalBetAmount;
+      checkAmount = false;
+   }
+} while(checkAmount);
+return 0;
+}
 
 
 
 int main(){
-   collectDeposit();
+  collectDeposit();
+   getBetAmount();
    return 0;
 }
